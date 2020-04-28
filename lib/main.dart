bf9cfb38a984e3523395f5d877c5dcfe446e9447
SmlingFarood <olatunde.abdulrahaman@gmail.com>
2020-04-28 10:20:20 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:shopapp/providers/cart.dart';
+import 'package:shopapp/providers/orders.dart';
+import 'package:shopapp/screens/cart_screen.dart';
+import 'package:shopapp/screens/edit_product_screen.dart';
+import 'package:shopapp/screens/orders_screen.dart';
+import 'package:shopapp/screens/product_detail_screen.dart';
+import 'package:shopapp/screens/product_overview_screen.dart';
+import 'package:shopapp/providers/products.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/screens/user_products_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Products(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Orders(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'My Shop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/providers/products.dart';
+import 'package:shopapp/screens/edit_product_screen.dart';
+import 'package:shopapp/widgets/app_drawer.dart';
+import 'package:shopapp/widgets/user_product_item.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Products'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productData.items.length,
+          itemBuilder: (_, i) => Column(
+            children: <Widget>[
+              UserProductItem(
+                productData.items[i].id,
+                productData.items[i].title,
+                productData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
